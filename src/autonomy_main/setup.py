d697b8ef543010bc25815c35581e80344d9efcbf
Krystian Soltys <krystian.soltys7@gmail.com>
2025-01-27 20:10:27 +0100

    Project structure adjusted for ROS2 Humble

--- src/autonomy_main/setup.py
+++ src/autonomy_main/setup.py
@@ -13,14 +13,13 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='ks',
-    maintainer_email='krystian.soltys7@gmail.com',
-    description='TODO: Package description',
+    maintainer='legendary',
+    maintainer_email='legendaryrover@to.do',
+    description='Rover 3.0 Autonomy Software',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            
             'publisher_node = autonomy_main.publisher_node:main',
             'listener_node = autonomy_main.listener_node:main',
         ],
